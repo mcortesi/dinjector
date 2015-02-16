76578c485a77fe4df1c662df64730f26ac02009a
Mariano A. Cortesi <mariano@zauberlabs.com>
2015-02-16 12:40:55 -0300

    Singleton Mapping Type
    Works for object creation and is cached by default

--- test/dummy/serviceA.js
+++ test/dummy/serviceA.js
@@ -1,10 +1,8 @@
 'use strict';
 
 class DummyService {
-  constructor(arg1, arg2, arg3) {
-    this.arg1 = arg1;
-    this.arg2 = arg2;
-    this.arg3 = arg3;
+  constructor(... args) {
+    this.args = args;
   }
 }
 
---
+++ test/types/singleton_type.test.js
@@ -0,0 +1,28 @@
+'use strict';
+/*jshint -W030 */
+
+var Immutable = require('immutable');
+var path = require('path');
+
+var { expect } = require('../test_commons');
+var SingletonType = require('../../lib/types/singleton_type');
+
+var requirePrefix = path.resolve(path.join(__dirname, "../.."));
+
+describe('SingletonType', function() {
+  var mappingType = null;
+
+  beforeEach(function () {
+    mappingType = new SingletonType(requirePrefix);
+  });
+
+  describe('#createObject()', function() {
+
+    it('should return a new instance of the object', function() {
+      var result = mappingType.createObject(Immutable.Map({path: '/test/dummy/serviceA', arguments: [21, 7]}), x => x);
+      expect(result.args).to.be.eql([21, 7]);
+    });
+
+  });
+
+});
