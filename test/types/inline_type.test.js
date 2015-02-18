c188260929610128d30854aa40a176dc96c7d76a
Mariano A. Cortesi <mariano@zauberlabs.com>
2015-02-18 12:52:31 -0300

    InlineType implemented

---
+++ test/types/inline_type.test.js
@@ -0,0 +1,34 @@
+'use strict';
+/*jshint -W030 */
+
+var Immutable = require('immutable');
+var path = require('path');
+
+var { expect } = require('../test_commons');
+var InlineType = require('../../lib/types/inline_type');
+
+var requirePrefix = path.resolve(path.join(__dirname, "../.."));
+
+describe('InlineType', function() {
+  var mappingType = null;
+
+  beforeEach(function () {
+    mappingType = new InlineType(requirePrefix);
+  });
+
+  describe('#createObject()', function() {
+
+    it('should return the result of calling createFn with the resolved arguments', function() {
+      var result = mappingType.createObject(
+        mappingType.preprocess(Immutable.Map({
+          type: 'inline',
+          arguments: [1,4,5],
+          createFn: (x1,x2,x3) => x1 + x2 + x3
+        })),
+        x => x * 2);
+      expect(result).to.be.eq(20);
+    });
+
+  });
+
+});
